3806bc88da5518716ab049fb3d43b4a5f6f684a3
142804583+alexjbrowntech@users.noreply.github.com <142804583+alexjbrowntech@users.noreply.github.com>
2023-09-15 11:44:17 +0100

    Player welcomed and named

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-/* Pseudo Code
-
-
+/*
 */
+
+//Welcome Players
+alert("Welcome to Rock Paper Scissors \n Two player game");
+
+//Player Name Prompts
+const playerOne = prompt("Player 1 Name");
+const playerTwo = prompt("Player 2 Name");
+
+// Alert for rules
+alert("Rules for Rock Paper Scissors:\n-Rock beats     Scissors\n-Scissors beats Paper\n-Paper beats Rock");
+alert("How to Play:\nType 'rock' 'paper' or 'scissors' on your turn.");
+
