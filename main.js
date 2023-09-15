a5a1f608c977991a506722525cfc7390458d2ef1
marcornc <mrc.ronchi@gmail.com>
2023-09-15 14:34:18 +0100

    add comments

--- main.js
+++ main.js
@@ -30,42 +30,48 @@ alert("How to Play:\nType 'rock' 'paper' or 'scissors' on your turn.");
 //Loop for move/input
 while(true){
 
+    //Loop to make sure that move1 is valid
   do {
     moveP1 = prompt(`${nameP1}\nChoose your move`);
     moveP1 = moveP1.toLowerCase();
   }
   while (!((moveP1 === "rock") || (moveP1 === "paper") || (moveP1 === "scissors")));
 
+  //Loop to make sure that move2 is valid
   do {
     moveP2 = prompt(`${nameP2}\nChoose your move`);
     moveP2 = moveP2.toLowerCase();
   }
   while (!((moveP2 === "rock") || (moveP2 === "paper") || (moveP2 === "scissors"))); 
 
-    if(moveP1 === "rock" && moveP2 === "scissors" || 
-    moveP1 === "scissors" && moveP2 === "paper" ||
-    moveP1 === "paper" && moveP2 === "rock") {
-        scoreP1 ++; 
-        alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP1} Wins!`);
+  //Condition to player1 wins
+    if(((moveP1 === "rock") && (moveP2 === "scissors")) ||
+        ((moveP1 === "scissors") && (moveP2 === "paper")) ||
+        ((moveP1 === "paper") && (moveP2 === "rock"))) {
+        
+            scoreP1 ++; 
 
-        alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+            alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP1} Wins!`);
+            alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
     }
-        else if(moveP1 === "rock" && moveP2 === "paper" || 
-        moveP1 === "scissors" && moveP2 === "rock" ||
-        moveP1 === "paper" && moveP2 === "scissors") {
-            scoreP2 ++;
-        alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP2} Wins!`);
-
-        alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+    //Condition to player2 wins
+        else if(((moveP1 === "rock") && (moveP2 === "paper")) || 
+                ((moveP1 === "scissors") && (moveP2 === "rock")) ||
+                ((moveP1 === "paper") && (moveP2 === "scissors"))) {
+                
+                    scoreP2 ++;
+                
+                    alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP2} Wins!`);
+                    alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
         }
-            else{
-              alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\nIt's a draw!`);
+        //Condition draw
+                else{
 
-              alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+                    alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\nIt's a draw!`);
+                    alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
             }
-    
-    console.log(`P1 score: ${scoreP1} P2 score: ${scoreP2}`);
-    
+
+    //Ask to continue or not, eventually goes out of the loop move/input
    let keepPlay = confirm("Do you want to continue?");
    if(keepPlay === false) break;
 }
