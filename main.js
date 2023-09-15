781b989643bcb39b4c035db9dd0aed4da49041be
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-09-15 12:31:51 +0100

    if function added to determine winner

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ Alex
 Marco
 Kit
 */
+
+//Set scores to 0
 let scoreP1 = 0;
 let scoreP2 = 0;
 //Welcome Players
@@ -22,11 +24,19 @@ while(true){
     let moveP1 = prompt(`${nameP1}\nChoose your move`);
     let moveP2 = prompt(`${nameP2}\nChoose your move`);
 
-    /*if(){player 1}
-        else if(){player2}
-            else{draw}*/
+    if(moveP1 === "rock" && moveP2 === "scissors" || 
+    moveP1 === "scissors" && moveP2 === "paper" ||
+    moveP1 === "paper" && moveP2 === "rock") {
+        scoreP1 ++; 
+    }
+        else if(moveP1 === "rock" && moveP2 === "paper" || 
+        moveP1 === "scissors" && moveP2 === "rock" ||
+        moveP1 === "paper" && moveP2 === "scissors") {
+            scoreP2 ++;
+        }
+            else{}
     
-
+    console.log(`P1 score: ${scoreP1} P2 score: ${scoreP2}`);
     
    let keepPlay = confirm("Do you want to continue?");
    if(keepPlay === false) break;
