6a1c8cae0a34742125a607b9c66f05f9d0510d8b
marcornc <mrc.ronchi@gmail.com>
2023-09-15 14:45:51 +0100

    create functionMove

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ let scoreP2 = 0;
 let moveP1;
 let moveP2;
 
+function functionMove(nameP){
+        //Loop to make sure that move1 is valid
+  do {
+    moveP = prompt(`${nameP}\nChoose your move`);
+    moveP = moveP.toLowerCase();
+  }
+  while (!((moveP === "rock") || (moveP === "paper") || (moveP === "scissors")));
+  return moveP;
+}
+
 //Welcome Players
 alert("Welcome to Rock Paper Scissors \n Two player game");
 
@@ -30,19 +40,8 @@ alert("How to Play:\nType 'rock' 'paper' or 'scissors' on your turn.");
 //Loop for move/input
 while(true){
 
-    //Loop to make sure that move1 is valid
-  do {
-    moveP1 = prompt(`${nameP1}\nChoose your move`);
-    moveP1 = moveP1.toLowerCase();
-  }
-  while (!((moveP1 === "rock") || (moveP1 === "paper") || (moveP1 === "scissors")));
-
-  //Loop to make sure that move2 is valid
-  do {
-    moveP2 = prompt(`${nameP2}\nChoose your move`);
-    moveP2 = moveP2.toLowerCase();
-  }
-  while (!((moveP2 === "rock") || (moveP2 === "paper") || (moveP2 === "scissors"))); 
+    moveP1 = functionMove(nameP1);
+    moveP2 = functionMove(nameP2);
 
   //Condition to player1 wins
     if(((moveP1 === "rock") && (moveP2 === "scissors")) ||
