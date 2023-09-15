6b2bfa41aa8443cd0b6e7aade1ed02f8d3a2a448
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-09-15 15:27:45 +0100

    added 1 player mode

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ let scoreP1 = 0;
 let scoreP2 = 0;
 let moveP1;
 let moveP2;
+let nameP1;
+let nameP2 = "computer";
+let gameType;
+
+//Function to ask players which move they make
 
 function functionMove(nameP){
         //Loop to make sure that move1 is valid
@@ -20,15 +25,52 @@ function functionMove(nameP){
   return moveP;
 }
 
+//Function to check who wins
+
+function functionCheck() {
+
+  //Condition to player1 wins
+  if(((moveP1 === "rock") && (moveP2 === "scissors")) ||
+  ((moveP1 === "scissors") && (moveP2 === "paper")) ||
+  ((moveP1 === "paper") && (moveP2 === "rock"))) {
+  
+      scoreP1 ++; 
+
+      alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP1} Wins!`);
+      alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+}
+//Condition to player2 wins
+  else if(((moveP1 === "rock") && (moveP2 === "paper")) || 
+          ((moveP1 === "scissors") && (moveP2 === "rock")) ||
+          ((moveP1 === "paper") && (moveP2 === "scissors"))) {
+          
+              scoreP2 ++;
+          
+              alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP2} Wins!`);
+              alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+  }
+  //Condition draw
+          else{
+
+              alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\nIt's a draw!`);
+              alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
+      }
+
+}
+
 //Welcome Players
 alert("Welcome to Rock Paper Scissors \n Two player game");
 
+gameType = prompt("Type '1' for a 1-player game \nType '2' for a two-player game");
+
+if (gameType == "2") {
+
 //Player Name Prompts
-const nameP1 = prompt("Player 1 Name");
+nameP1 = prompt("Player 1 Name");
 if (nameP1 === null) {
   nameP1 = "Player 1";
 }
-const nameP2 = prompt("Player 2 Name");
+nameP2 = prompt("Player 2 Name");
 if (nameP2 === null) {
   nameP2 = "Player 1";
 }
@@ -43,34 +85,56 @@ while(true){
     moveP1 = functionMove(nameP1);
     moveP2 = functionMove(nameP2);
 
-  //Condition to player1 wins
-    if(((moveP1 === "rock") && (moveP2 === "scissors")) ||
-        ((moveP1 === "scissors") && (moveP2 === "paper")) ||
-        ((moveP1 === "paper") && (moveP2 === "rock"))) {
-        
-            scoreP1 ++; 
-
-            alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP1} Wins!`);
-            alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
-    }
-    //Condition to player2 wins
-        else if(((moveP1 === "rock") && (moveP2 === "paper")) || 
-                ((moveP1 === "scissors") && (moveP2 === "rock")) ||
-                ((moveP1 === "paper") && (moveP2 === "scissors"))) {
-                
-                    scoreP2 ++;
-                
-                    alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\n${nameP2} Wins!`);
-                    alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
-        }
-        //Condition draw
-                else{
-
-                    alert(`${nameP1} played ${moveP1}, ${nameP2} played ${moveP2}\nIt's a draw!`);
-                    alert(`${nameP1}'s score: ${scoreP1} - ${nameP2}'s score: ${scoreP2}`);
-            }
+    functionCheck();
 
     //Ask to continue or not, eventually goes out of the loop move/input
    let keepPlay = confirm("Do you want to continue?");
    if(keepPlay === false) break;
 }
+}
+
+if (gameType == "1") {
+    //Player Name Prompts
+    nameP1 = prompt("Player 1 Name");
+    if (nameP1 === null) {
+    nameP1 = "Player 1";
+    }
+    nameP2 = "computer";
+}
+
+//Loop for move/input
+while(true){
+
+    moveP1 = functionMove(nameP1);
+    // Generate a random decimal number between 0 (inclusive) and 1 (exclusive)
+const randomDecimal = Math.random();
+// Map the random decimal to 1, 2, or 3
+let randomNumber;
+if (randomDecimal < 0.33) {
+  randomNumber = 1;
+} else if (randomDecimal < 0.67) {
+  randomNumber = 2;
+} else {
+  randomNumber = 3;
+}
+    switch(randomNumber) {
+        case 1:
+            moveP2 = "rock";
+            break;
+        case 2: 
+            moveP2 = "paper";
+            break;
+        case 3:
+            moveP2 = "scissors";
+            break;
+    }
+
+    functionCheck();
+
+    //Ask to continue or not, eventually goes out of the loop move/input
+   let keepPlay = confirm("Do you want to continue?");
+   if(keepPlay === false) break;
+
+
+}
+
